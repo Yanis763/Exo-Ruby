5a05a22008f3c47f7e8f8f6c973fc2e33698af3b
Yanis763 <yanispsn76@hotmail.com>
2020-09-29 01:19:34 +0200

    Exo du soir

---
+++ exo_01.rb
@@ -0,0 +1,11 @@
+puts "Bonjour, monde !"
+
+nom = gets.chomp
+puts "Salut #{nom}, elle est vachement grande ta bite"
+
+nb = gets
+!nb = nb
+
+if !nb == nb
+    puts "C'est un palyndrome"
+end
---
+++ exo_05.rb
@@ -0,0 +1,25 @@
+#Affiche un string
+puts "On va compter le nombre d'heures de travail à THP"
+#puts affiche un string et #{} effectue les multiplications
+puts "Travail : #{10 * 5 * 11}"
+#puts affiche un string et #{} effectue les multiplications
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#Affiche un string
+puts "Et en secondes ?"
+#Effectue les multiplications
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
