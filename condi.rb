d7ce1abccc432dcc8290870c1c77cbe6d6558155
Yanis763 <yanispsn76@hotmail.com>
2020-09-30 22:59:19 +0200

    cc

---
+++ condi.rb
@@ -0,0 +1,16 @@
+puts "What is your major?"
+major = gets.chomp
+
+case major
+when "Biology"
+	puts "Mmm the study of life itself!"
+when "Computer Science"
+	puts "I'm a computer!"
+when "English"
+	puts "No way! What's your favorite book?"
+when "Math"
+	puts "Sweet! I'm great with numbers!"
+else
+	puts "That's a cool major!"
+end
+
